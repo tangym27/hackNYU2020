edd06ddca707257bed89c3ff522099dd75829b6f
kairuihuang <6kairuih@gmail.com>
2020-03-07 17:11:22 -0500

    added user db requests

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import sqlite3
+from sqlite3 import Error
 from flask import Flask
 from flask import g
 
 app = Flask(__name__)
+DATABASE = '/data/DAC.db'
 
 @app.route('/')
 def hello_world():
@@ -16,4 +18,32 @@ def greet(name):
 def decimal(number):
     return 'The URL you entered returned %f' % number
 
-app.run(debug=True)
+@app.route('/users/<int:id>')
+def single_user():
+    return get_users_by_ID(get_db())
+# ________________________________
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def get_users(db):
+    cur = db.cursor()
+    cur.execute('SELECT * FROM usersAccounts')
+    return cur.fetchall()
+
+def get_users_by_ID(db, id):
+    cur = db.cursor()
+    cur.execute('SELECT * FROM userAccounts WHERE id = @id')
+    return cur.fetchall();
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    db = get_db();
+    app.run(debug = True)
